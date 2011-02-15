f95000c6afe3e9d0a86a9763ac5f5c587acce1b7
Grigoriy Petukhov <lorien@lorien.name>
2011-02-15 20:47:33 +0600

    Add comittee app which holds Committee and Subcommitte models. Create committees data parser

---
+++ committee/admin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8
+
+from django.contrib import admin
+from committee.models import Committee, Subcommittee
+
+class SubcommitteeInline(admin.TabularInline):
+    model = Subcommittee
+
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'committee_type', 'code', 'abbrev']
+    inlines = [SubcommitteeInline]
+    search_fields = ['name', 'code']
+
+class SubcommitteeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'committee', 'code']
+
+
+admin.site.register(Committee, CommitteeAdmin)
+admin.site.register(Subcommittee, SubcommitteeAdmin)
--- parser/processor.py
+++ parser/processor.py
@@ -1,19 +1,44 @@
 class Processor(object):
     FIELD_MAPPING = {}
+    TYPO_MAPPING = {}
+
+    def check_required_key(self, key, node):
+        """
+        If key is required and does not exist in the XML node
+        then raise Exception.
+        """
+
+        if key in self.REQUIRED_ATTRIBUTES:
+            if key in node.attrib:
+                return
+            if key in self.TYPO_MAPPING:
+                key2 = self.TYPO_MAPPING[key]
+                if key2 in node.attrib:
+                    return
+            raise Exception('Did not found required attribute %s in record %s' % (
+                key, self.display_node(node)))
 
     def process(self, obj, node):
+        # o_O
+        # So we check here that XML node has all required attribute
+        # If some attribute does not exist then we give it second chance:
+        # some attributes are stored with misspelled name so we use TYPO_MAPPING
+        # to check this
         for key in self.ATTRIBUTES:
-            if key in self.REQUIRED_ATTRIBUTES:
-                if not key in node.attrib:
-                    raise Exception('Did not found required attribute %s in record %s' % (
-                        key, self.display_node(node)))
+            self.check_required_key(key, node)
             if key in node.attrib:
                 field_name = self.FIELD_MAPPING.get(key, key)
                 setattr(obj, field_name, self.convert(key, node.get(key)))
+            else:
+                if key in self.TYPO_MAPPING:
+                    key2 = self.TYPO_MAPPING[key]
+                    if key2 in node.attrib:
+                        field_name = self.FIELD_MAPPING.get(key, key)
+                        setattr(obj, field_name, self.convert(key, node.get(key2)))
         return obj
 
     def display_node(self, node):
-        return ', '.join('%s: %s' % x for x in node.attrib.iteritems())
+        return '<%s>: ' % node.tag + ', '.join('%s: %s' % x for x in node.attrib.iteritems())
 
     def convert(self, key, value):
         if hasattr(self, '%s_handler' % key):
