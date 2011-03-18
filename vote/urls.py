85fe1a61105eacaf11571b962ccc39ae2adff661
Grigoriy Petukhov <lorien@lorien.name>
2011-03-18 09:12:00 +0600

    Add export links to Vote Details page

--- vote/urls.py
+++ vote/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('vote.views',
     url('^congress/votes$', 'vote_list', name='vote_list'),
     url('^congress/votes/(\d+)-(\w+)/(h|s)(\d+)$', 'vote_details', name='vote_details'),
+    url('^congress/votes/(\d+)-(\w+)/(h|s)(\d+)/export/csv$', 'vote_export_csv', name='vote_export_csv'),
+    url('^congress/votes/(\d+)-(\w+)/(h|s)(\d+)/export/xml$', 'vote_export_xml', name='vote_export_xml'),
 )
--- vote/views.py
+++ vote/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import csv
+from StringIO import StringIO
+
 from django.shortcuts import redirect, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 
 from common.decorators import render_to
 from common.pagination import paginate
@@ -26,17 +30,49 @@ def vote_list(request):
             }
 
 
-@render_to('vote/vote_details.html')
-def vote_details(request, congress, session, chamber_code, number):
+def load_vote(congress, session, chamber_code, number):
+    """
+    Helper utility to get `Vote` instance by arguments
+    provided in the request.
+    """
+
     if chamber_code == 'h':
         chamber = CongressChamber.house
     else:
         chamber = CongressChamber.senate
-    vote = get_object_or_404(Vote, congress=congress, session=session,
+    return get_object_or_404(Vote, congress=congress, session=session,
                              chamber=chamber, number=number)
+
+
+@render_to('vote/vote_details.html')
+def vote_details(request, congress, session, chamber_code, number):
+    vote = load_vote(congress, session, chamber_code, number)
     voters = list(vote.voters.all().select_related('person', 'option'))
     load_roles_at_date([x.person for x in voters], vote.created)
     return {'vote': vote,
             'voters': voters,
             'CongressChamber': CongressChamber,
             }
+
+
+def vote_export_csv(request, congress, session, chamber_code, number):
+    vote = load_vote(congress, session, chamber_code, number)
+    voters = vote.voters.all().select_related('person', 'option')
+    load_roles_at_date([x.person for x in voters], vote.created)
+
+    outfile = StringIO()
+    writer = csv.writer(outfile)
+    for voter in voters:
+        writer.writerow([voter.person.pk, voter.person.role.state, voter.person.role.district,
+                         voter.option.value, voter.person.name_no_district().encode('utf-8')])
+    output = outfile.getvalue()
+    firstline = '%s Vote #%d %s - %s\n' % (vote.get_chamber_display(), vote.number,
+                                         vote.created.strftime('%b %d, %Y'), vote.question)
+    firstline = firstline.encode('utf-8')
+    return HttpResponse(firstline + output, content_type='text/csv')
+
+
+def vote_export_xml(request, congress, session, chamber_code, number):
+    vote = load_vote(congress, session, chamber_code, number)
+    fobj = open('data/us/%s/rolls/%s%s-%s.xml' % (congress, chamber_code, session, number))
+    return HttpResponse(fobj, content_type='text/xml')
