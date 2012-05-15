f99339cd3582ad44d4ec88f85d90c4dc9637242f
Joshua Tauberer <tauberer@govtrack.us>
2012-05-15 10:09:45 -0400

    recorganized developer docs and made room for API docs

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'index', name='index'),
-    url(r'^(overview|about|press|sources|advertising|developers|developers/downstream|developers/data|developers/license|civicimpulse|blog_template)$', 'staticpage', name='staticpage'),
+    url(r'^(overview|about|press|sources|advertising|developers|developers/downstream|developers/data|developers/license|developers/rsync|developers/api|developers/people_xml|developers/vote_xml|developers/bill_xml|civicimpulse|blog_template)$', 'staticpage', name='staticpage'),
     url(r'^congress/?$', 'congress_home', name='congress_home'),
     url(r'^search$', 'search', name='search'),
     url(r'^campaigns/bulkdata', 'campaign_bulk_data'),
