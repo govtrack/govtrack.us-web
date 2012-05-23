608e62c3595a4093c6362f4492ecbf64751b9c0d
Joshua Tauberer <tauberer@govtrack.us>
2012-05-22 23:47:35 -0400

    show leg staffers WMATA info in the ad slot after 4pm

--- website/middleware.py
+++ website/middleware.py
@@ -1,4 +1,8 @@
 from models import Req
+from settings import WMATA_API_KEY
+from django.core.cache import cache
+ 
+import urllib, json, datetime
 
 # http://whois.arin.net/rest/org/ISUHR/nets
 HOUSE_NET_RANGES = (
@@ -11,8 +15,8 @@ HOUSE_NET_RANGES = (
     )
 # http://whois.arin.net/rest/org/USSAA/nets
 SENATE_NET_RANGES = (
-	("156.33.0.0", "156.33.255.255"),
-	)
+    ("156.33.0.0", "156.33.255.255"),
+    )
 
 def template_context_processor(request):
     context = { }
@@ -21,9 +25,9 @@ def template_context_processor(request):
     # House or Senate netblocks.
     
     def ip_to_quad(ip):
-    	return [int(s) for s in ip.split(".")]
+        return [int(s) for s in ip.split(".")]
     def compare_ips(ip1, ip2):
-    	return cmp(ip_to_quad(ip1), ip_to_quad(ip2))
+        return cmp(ip_to_quad(ip1), ip_to_quad(ip2))
     def is_ip_in_range(ip, block):
        return compare_ips(ip, block[0]) >= 0 and compare_ips(ip, block[1]) <= 0
     def is_ip_in_any_range(ip, blocks):
@@ -33,12 +37,30 @@ def template_context_processor(request):
        return False
     
     try:
-    	if is_ip_in_any_range(request.META["REMOTE_ADDR"], HOUSE_NET_RANGES):
-    		context["remote_net_house"] = True
-    	if is_ip_in_any_range(request.META["REMOTE_ADDR"], SENATE_NET_RANGES):
-    		context["remote_net_senate"] = True
+        if is_ip_in_any_range(request.META["REMOTE_ADDR"], HOUSE_NET_RANGES):
+            context["remote_net_house"] = True
+        if is_ip_in_any_range(request.META["REMOTE_ADDR"], SENATE_NET_RANGES):
+            context["remote_net_senate"] = True
     except:
-    	pass
+        pass
+    
+    # Get WMATA train status at Capitol South and Union Station.
+    def wmata_info():
+        if datetime.datetime.now().hour < 16: return { }
+        ret = cache.get("hill_wmata_info")
+        if ret: return ret
+        ret = { }
+        try:
+            ret.update( json.loads(urllib.urlopen("http://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03,D05?api_key=" + WMATA_API_KEY).read()) )
+            ret.update( json.loads(urllib.urlopen("http://api.wmata.com/Incidents.svc/json/Incidents?api_key=" + WMATA_API_KEY).read()) )
+            ret["Trains"] = [t for t in ret["Trains"] if t["Line"] in ("RD", "BL", "OR")] # exclude no passengers
+            for inc in ret["Incidents"]:
+                inc["LinesAffected"] = [t for t in inc.get("LinesAffected", "").split(";") if t != ""]
+        except:
+            pass
+        cache.set("hill_wmata_info", ret, 15) # 15 seconds
+        return ret
+    context["wmata_info"] = wmata_info
     
     return context
     
