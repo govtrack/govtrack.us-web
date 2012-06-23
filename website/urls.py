3c87a3e3bf917ae34abbb35027000ab97d0e3db9
Joshua Tauberer <tauberer@govtrack.us>
2012-06-23 15:38:41 -0400

    change Lists => Track and add a new Other Tools main menu item

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('website.views',
     url(r'^$', 'index', name='index'),
-    url(r'^(overview|about|press|sources|advertising|developers|developers/downstream|developers/data|developers/license|developers/rsync|developers/people_xml|developers/vote_xml|developers/bill_xml|civicimpulse|blog_template)$', 'staticpage', name='staticpage'),
+    url(r'^(overview|about|press|sources|advertising|tools|developers|developers/downstream|developers/data|developers/license|developers/rsync|developers/people_xml|developers/vote_xml|developers/bill_xml|civicimpulse|blog_template)$', 'staticpage', name='staticpage'),
     url(r'^developers/api$', 'api_overview'),
     url(r'^congress/?$', 'congress_home', name='congress_home'),
     url(r'^search$', 'search', name='search'),
