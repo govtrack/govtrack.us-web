b480c617e32523152d7143e37609123511fd55d9
Joshua Tauberer <tauberer@govtrack.us>
2012-06-26 14:53:19 -0400

    move the live video thumbnails into a 'dynamic' directory and hg-ignore it

--- website/misc/make_floor_video_thumbs.sh
+++ website/misc/make_floor_video_thumbs.sh
@@ -2,12 +2,14 @@
 
 # Run from the web source directory.
 
+mkdir -p static/images/dynamic
+
 rm -f /tmp/00000001.jpg
 nohup mplayer -really-quiet -noconsolecontrols -nosound -vo jpeg:outdir=/tmp -frames 1 -nocache mmsh://grani-senateenc01.wm.llnwd.net/grani_senateenc01 > /dev/null 2&>/dev/null
-mv /tmp/00000001.jpg static/images/floor_thumb_senate.jpeg
+mv /tmp/00000001.jpg static/images/dynamic/floor_thumb_senate.jpeg
 
 rm -f /tmp/00000001.jpg
 nohup mplayer -really-quiet -noconsolecontrols -nosound -vo jpeg:outdir=/tmp -frames 1 -nocache mmsh://grani-househ264enc01.wm.llnwd.net/grani_houseH264ENC01 > /dev/null 2&>/dev/null
-mv /tmp/00000001.jpg static/images/floor_thumb_house.jpeg
+mv /tmp/00000001.jpg static/images/dynamic/floor_thumb_house.jpeg
 
 
