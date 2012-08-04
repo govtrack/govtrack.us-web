02a1800d59d81d161f9cdbba6aaff40b68d656d0
Joshua Tauberer <tauberer@govtrack.us>
2012-08-04 15:13:01 -0400

    more Django 1.4 updates

--- settings.py
+++ settings.py
@@ -72,9 +72,8 @@ EMAIL_HOST = 'localhost'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 if not DEBUG:
     TEMPLATE_LOADERS = (
@@ -136,7 +135,7 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
