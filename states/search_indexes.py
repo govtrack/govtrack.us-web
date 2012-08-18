46a54c5bcef647995bf19842e469ce9299ef4583
Joshua Tauberer <jt@occams.info>
2012-08-18 19:13:02 -0400

    remove dead code

--- states/search_indexes.py
+++ states/search_indexes.py
@@ -1,14 +1,5 @@
 from models import StateBill
 from smartsearch import build_haystack_index
-from haystack.routers import BaseRouter
 
 StateBillIndex = build_haystack_index(StateBill)
 
-class MyRouter(BaseRouter):
-    def for_read(self, **hints):
-        raise ValueError(repr(hints))
-        return None
-
-    def for_write(self, **hints):
-        raise ValueError(repr(hints))
-        return None
