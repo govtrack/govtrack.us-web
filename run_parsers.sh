5c3851f5a427041b5082e09976a7d6521f88078b
Joshua Tauberer <tauberer@govtrack.us>
2012-08-24 19:01:05 -0400

    okay now try Solr instead of ElasticSearch, since ES was giving me unreliable/random results, but we'll keep using xapian for people and state bills for now
    --HG--
    branch : elasticsearch

--- run_parsers.sh
+++ run_parsers.sh
@@ -9,11 +9,9 @@ export RELEASE=1
 ./manage.py update_index -v 0 -u person person
 
 # bills and state bills are indexed as they are parsed, but to
-# freshen the index:
+# freshen the index. Because bills index full text and so
+# indexing each time is substantial, set the TIMEOUT and
+# BATCH_SIZE options in the haystack connections appropriately.
 # ./manage.py update_index -v 2 -u bill bill
 # ./manage.py update_index -v 2 -u states states
-
-# To clear an index, access elasticsearch directly. Haystack
-# has a problem deleting an index. e.g.:
-# curl -XDELETE 'http://localhost:9200/person'
 
