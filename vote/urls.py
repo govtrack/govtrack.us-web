59d5b6db1c64a7b2cc2f8bbbb841187d91ca108e
Joshua Tauberer <tauberer@govtrack.us>
2013-01-29 16:22:22 -0500

    vote thumbnails for og:image

--- vote/urls.py
+++ vote/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('vote.views',
     url('^votes/(\d+)-(\w+)/(h|s)(\d+)$', 'vote_details', name='vote_details'),
     url('^votes/(\d+)-(\w+)/(h|s)(\d+)/export/csv$', 'vote_export_csv', name='vote_export_csv'),
     url('^votes/(\d+)-(\w+)/(h|s)(\d+)/export/xml$', 'vote_export_xml', name='vote_export_xml'),
+    url('^votes/(\d+)-(\w+)/(h|s)(\d+)/thumbnail$', 'vote_thumbnail_image', name='vote_thumbnail_image'),
 )
