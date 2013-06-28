ce238bb78587382fccf194f0f192817e618676ae
Joshua Tauberer <jt@occams.info>
2013-06-28 07:07:47 -0400

    in top-tracked report, also show top bills tracked by newly registered users (as a proxy for when the tracker was added, which we weren't tracking until just now -- I've added a timestamp column in the database, but not yet in the Django model)

--- website/management/commands/top_tracked_bills.py
+++ website/management/commands/top_tracked_bills.py
@@ -5,15 +5,29 @@ from django.db.models import Count
 from events.models import Feed
 from bill.models import Bill
 
+import datetime
+
 class Command(BaseCommand):
 	args = ''
 	help = 'Reports top-tracked bills.'
 	
 	def handle(self, *args, **options):
+		print "tracked by users registered in the last two weeks"
+		self.show_stats(True)
+
+		print
+		print "tracked by all users"
+		self.show_stats(False)
+	
+	def show_stats(self, recent_users_only):
 		# get feeds, across all congresses
-		top_bills = Feed.objects.annotate(count=Count('tracked_in_lists'))\
+		top_bills = Feed.objects\
 			.filter(feedname__startswith='bill:')\
-			.filter(feedname__regex='^bill:[hs][jcr]?%d-' % settings.CURRENT_CONGRESS)\
+			.filter(feedname__regex='^bill:[hs][jcr]?%d-' % settings.CURRENT_CONGRESS)
+		if recent_users_only:
+			top_bills = top_bills.filter(tracked_in_lists__user__date_joined__gt=datetime.datetime.now()-datetime.timedelta(days=14))
+		top_bills = top_bills\
+			.annotate(count=Count('tracked_in_lists'))\
 			.order_by('-count')\
 			.values('feedname', 'count')\
 			[0:25]
