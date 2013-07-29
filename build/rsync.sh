d10cab295dfa243d7aada6cf4dbcf10d593bd1ea
Joshua Tauberer <jt@occams.info>
2013-07-29 18:17:41 -0400

    in build/rsync.sh, exclude the congress and congress-legislators data directories (for now at least)

--- build/rsync.sh
+++ build/rsync.sh
@@ -4,4 +4,6 @@ rsync -avz --delete --delete-excluded govtrack.us::govtrackdata data \
 	--exclude "**/repstats" --exclude "**/repstats.person" \
 	--exclude "**/bills.amdt" --exclude "**/bills.cbo" --exclude "**/bills.ombsap" \
 	--exclude "photos" \
+	--exclude "congress" \
+	--exclude "congress-legislators" \
 	--include "us/113" --exclude "us/[0-9]*"
