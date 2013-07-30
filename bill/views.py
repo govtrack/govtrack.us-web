764f3853399e8a8ee46af0cf21630d4437151d69
Gordon P. Hemsley <me@gphemsley.org>
2013-07-30 14:34:16 -0400

    Start widget.
    Conflicts:
    	bill/views.py

--- bill/views.py
+++ bill/views.py
@@ -36,13 +36,13 @@ def bill_details(request, congress, type_slug, number):
             bill_type = BillType.by_slug(type_slug)
         except BillType.NotFound:
             raise Http404("Invalid bill type: " + type_slug)
-    
+
     bill = get_object_or_404(Bill, congress=congress, bill_type=bill_type, number=number)
-    
+
     from person.name import get_person_name
     sponsor_name = None if not bill.sponsor else \
         get_person_name(bill.sponsor, role_date=bill.introduced_date, firstname_position='before', show_suffix=True)
-    
+
     def get_reintroductions():
         reintro_prev = None
         reintro_next = None
@@ -50,7 +50,7 @@ def bill_details(request, congress, type_slug, number):
             if reintro.congress < bill.congress: reintro_prev = reintro
             if reintro.congress > bill.congress and not reintro_next: reintro_next = reintro
         return reintro_prev, reintro_next
-        
+
     def get_text_info():
         from models import USCSection
         from billtext import load_bill_text
@@ -58,7 +58,7 @@ def bill_details(request, congress, type_slug, number):
         import re
         try:
             metadata = load_bill_text(bill, None, mods_only=True)
-            
+
             # do interesting stuff with citations
             if "citations" in metadata:
                 slip_laws = []
@@ -81,17 +81,17 @@ def bill_details(request, congress, type_slug, number):
                             # create a fake entry for the sake of output
                             # the 'id' field is set to make these objects properly hashable
                             sec_obj = USCSection(id=cite["text"], name=cite["text"], parent_section=usc_other)
-                        
+
                         if "range_to_section" in cite:
                             sec_obj.range_to_section = cite["range_to_section"]
-                        
+
                         # recursively go up to the title
                         path = [sec_obj]
                         so = sec_obj
                         while so.parent_section:
                             so = so.parent_section
                             path.append(so)
-                            
+
                         # build a link to LII
                         if cite["type"] == "usc-section":
                             cite_link = "http://www.law.cornell.edu/uscode/text/" + cite["title"]
@@ -103,7 +103,7 @@ def bill_details(request, congress, type_slug, number):
                                 (so.level_type + "-" + so.number) for so in reversed(path[:-1])
                                 )
                         sec_obj.link = cite_link
-                        
+
                         # now pop off from the path to put the node at the right point in a tree
                         container = usc
                         while path:
@@ -113,9 +113,9 @@ def bill_details(request, congress, type_slug, number):
 
                     else:
                         other.append(cite)
-                        
+
                 slip_laws.sort(key = lambda x : (x["congress"], x["number"]))
-                
+
                 # restructure data format
                 def ucfirst(s): return s[0].upper() + s[1:]
                 def rebuild_usc_sec(seclist, indent=0):
@@ -131,7 +131,7 @@ def bill_details(request, congress, type_slug, number):
                         ret.extend(rebuild_usc_sec(subparts, indent=indent+1))
                     return ret
                 usc = rebuild_usc_sec(usc)
-                
+
                 metadata["citations"] = {
                     "slip_laws": slip_laws, "statutes": statutes, "usc": usc, "other": other,
                     "count": len(slip_laws)+len(statutes)+len(usc)+len(other) }
@@ -149,7 +149,7 @@ def bill_details(request, congress, type_slug, number):
         "dead": bill.congress != CURRENT_CONGRESS and bill.current_status not in BillStatus.final_status_obvious,
         "feed": Feed.BillFeed(bill),
         "text": get_text_info,
-        
+
         "care2_category_id": {
             5816: '793', # Agriculture and Food=>Health
             5840: '789', # Animals=>Animal Welfare
@@ -173,7 +173,7 @@ def bill_details_user_view(request, congress, type_slug, number):
     except BillType.NotFound:
         raise Http404("Invalid bill type: " + type_slug)
     bill = get_object_or_404(Bill, congress=congress, bill_type=bill_type, number=number)
-    
+
     ret = { }
     if request.user.is_staff:
         admin_panel = """
@@ -190,11 +190,52 @@ def bill_details_user_view(request, congress, type_slug, number):
             'bill': bill,
             "feed": Feed.BillFeed(bill),
             }))
-    
+
     from person.views import render_subscribe_inline
     ret.update(render_subscribe_inline(request, Feed.BillFeed(bill)))
-    
+
     return ret
+
+@anonymous_view
+@render_to('bill/bill_widget.html')
+def bill_widget(request, congress, type_slug, number):
+    try:
+        bill_type = BillType.by_slug(type_slug)
+    except BillType.NotFound:
+        raise Http404("Invalid bill type: " + type_slug)
+
+    bill = get_object_or_404(Bill, congress=congress, bill_type=bill_type, number=number)
+
+    from person.name import get_person_name
+    sponsor_name = None if not bill.sponsor else \
+        get_person_name(bill.sponsor, role_date=bill.introduced_date, firstname_position='before', show_suffix=True)
+
+    def get_reintroductions():
+        reintro_prev = None
+        reintro_next = None
+        for reintro in bill.find_reintroductions():
+            if reintro.congress < bill.congress: reintro_prev = reintro
+            if reintro.congress > bill.congress and not reintro_next: reintro_next = reintro
+        return reintro_prev, reintro_next
+
+    def get_text_info():
+        from billtext import load_bill_text
+        try:
+            return load_bill_text(bill, None, mods_only=True)
+        except IOError:
+            return None
+
+    return {
+        'bill': bill,
+        "congressdates": get_congress_dates(bill.congress),
+        "subtitle": get_secondary_bill_title(bill, bill.titles),
+        "sponsor_name": sponsor_name,
+        "reintros": get_reintroductions, # defer so we can use template caching
+        "current": bill.congress == CURRENT_CONGRESS,
+        "dead": bill.congress != CURRENT_CONGRESS and bill.current_status not in BillStatus.final_status_obvious,
+        "feed": Feed.BillFeed(bill),
+        "text": get_text_info,
+    }
 
 @json_response
 @login_required
@@ -203,7 +244,7 @@ def market_test_vote(request):
     prediction = int(request.POST.get("prediction", "0"))
     market = bill.get_open_market(request.user)
     if not market: return { }
-    
+
     from predictionmarket.models import Trade, TradingAccount
     account = TradingAccount.get(request.user)
     if prediction != 0:
@@ -212,13 +253,13 @@ def market_test_vote(request):
             t = Trade.place(account, market.outcomes.get(owner_key = 1 if prediction == 1 else 0), 10)
         except ValueError as e:
             return { "error": str(e) }
-            
+
     else:
         # Sell shares.
         positions, pl = account.position_in_market(market)
         for outcome in positions:
             Trade.place(account, outcome, -positions[outcome]["shares"])
-    
+
     return { "vote": prediction }
 
 @anonymous_view
@@ -226,13 +267,13 @@ def market_test_vote(request):
 def bill_text(request, congress, type_slug, number, version=None):
     if version == "":
         version = None
-    
+
     try:
         bill_type = BillType.by_slug(type_slug)
     except BillType.NotFound:
         raise Http404("Invalid bill type: " + type_slug)
     bill = get_object_or_404(Bill, congress=congress, bill_type=bill_type, number=number)
-    
+
     from billtext import load_bill_text, bill_gpo_status_codes
     try:
         textdata = load_bill_text(bill, version)
@@ -278,22 +319,22 @@ def bill_text_ajax(request):
     for p in ("left_bill", "left_version", "right_bill", "right_version", "mode"):
         if not p in request.GET:
             raise Http404()
-            
+
     try:
         return load_comparison(request.GET["left_bill"], request.GET["left_version"], request.GET["right_bill"], request.GET["right_version"])
     except IOError:
         return { "error": "Bill text is not available for those bills." }
-    
+
 def load_comparison(left_bill, left_version, right_bill, right_version, timelimit=10, force=False):
     from billtext import load_bill_text, compare_xml_text, get_current_version
     import lxml
-    
+
     left_bill = Bill.objects.get(id = left_bill)
     right_bill = Bill.objects.get(id = right_bill)
-    
+
     if left_version == "": left_version = get_current_version(left_bill)
     if right_version == "": right_version = get_current_version(right_bill)
-    
+
     btc = None
     try:
         btc = BillTextComparison.objects.get(
@@ -305,7 +346,7 @@ def load_comparison(left_bill, left_version, right_bill, right_version, timelimi
         if not force: return btc.data
     except BillTextComparison.DoesNotExist:
         pass
-    
+
     # Try with the bills swapped.
     try:
         btc2 = BillTextComparison.objects.get(
@@ -323,25 +364,25 @@ def load_comparison(left_bill, left_version, right_bill, right_version, timelimi
         }
     except BillTextComparison.DoesNotExist:
         pass
-    
+
     left = load_bill_text(left_bill, left_version, mods_only=True)
     right = load_bill_text(right_bill, right_version, mods_only=True)
-    
+
     doc1 = lxml.etree.parse(left["basename"] + ".html")
     doc2 = lxml.etree.parse(right["basename"] + ".html")
     compare_xml_text(doc1, doc2, timelimit=timelimit) # revises DOMs in-place
-    
+
     # dates aren't JSON serializable
     left["docdate"] = left["docdate"].strftime("%x")
     right["docdate"] = right["docdate"].strftime("%x")
-    
+
     ret = {
         "left_meta": left,
         "right_meta": right,
         "left_text": lxml.etree.tostring(doc1),
         "right_text": lxml.etree.tostring(doc2),
     }
-    
+
     if not btc:
         btc = BillTextComparison(
             bill1 = left_bill,
@@ -351,10 +392,10 @@ def load_comparison(left_bill, left_version, right_bill, right_version, timelimi
             data = dict(ret)) # clone before compress()
     else:
         btc.data = dict(ret) # clone before compress()
-        
+
     btc.compress()
     btc.save()
-    
+
     return ret
 
 def bill_list(request):
@@ -365,7 +406,7 @@ def bill_list(request):
             def get_redirect_response():
                 return { "redirect": bill.get_absolute_url() }
             return get_redirect_response()
-    
+
     ix1 = None
     ix2 = None
     if "subject" in request.GET:
@@ -392,17 +433,17 @@ def show_bill_browse(template, request, ix1, ix2, context):
         noun = ("bill", "bills"),
         context = context,
         )
- 
+
 def query_popvox(method, args):
     if isinstance(method, (list, tuple)):
         method = "/".join(method)
-    
+
     _args = { }
     if args != None: _args.update(args)
     _args["api_key"] = settings.POPVOX_API_KEY
-    
+
     url = "https://www.popvox.com/api/" + method + "?" + urllib.urlencode(_args).encode("utf8")
-    
+
     req = urllib2.Request(url)
     resp = urllib2.urlopen(req)
     if resp.getcode() != 200:
@@ -437,7 +478,7 @@ bill_status_groups = [
         "bills and joint/concurrent resolutions", " that had a significant vote in one chamber, making them likely to have further action", " that had a significant vote in one chamber",
         (BillStatus.pass_over_house, BillStatus.pass_over_senate, BillStatus.pass_back_senate, BillStatus.pass_back_house, BillStatus.passed_bill)), # 5
     ("Failed Legislation",
-        "bills and resolutions", " that failed a vote on passage and are now dead or failed a significant vote such as cloture, passage under suspension, or resolving differences", " that failed a vote on passage or failed a significant vote such as cloture, passage under suspension, or resolving differences", 
+        "bills and resolutions", " that failed a vote on passage and are now dead or failed a significant vote such as cloture, passage under suspension, or resolving differences", " that failed a vote on passage or failed a significant vote such as cloture, passage under suspension, or resolving differences",
         (BillStatus.fail_originating_house, BillStatus.fail_originating_senate, BillStatus.fail_second_house, BillStatus.fail_second_senate, BillStatus.prov_kill_suspensionfailed, BillStatus.prov_kill_cloturefailed, BillStatus.prov_kill_pingpongfail)), # 7
     ("Vetoed Bills (w/o Override)",
         "bills", " that were vetoed and the veto was not overridden by Congress", " that were vetoed and the veto was not overridden by Congress",
@@ -455,7 +496,7 @@ def load_bill_status_qs(statuses, congress=CURRENT_CONGRESS):
 def bill_docket(request):
     def build_info():
         feeds = [f for f in Feed.get_simple_feeds() if f.category == "federal-bills"]
-        
+
         groups = [
             (   g[0], # title
                 g[1], # text 1
@@ -465,20 +506,20 @@ def bill_docket(request):
                load_bill_status_qs(g[4]).order_by('-current_status_date')[0:6], # top 6 in this category
                 )
             for g in bill_status_groups ]
-            
+
         dhg_bills = Bill.objects.filter(congress=CURRENT_CONGRESS, docs_house_gov_postdate__gt=datetime.datetime.now() - datetime.timedelta(days=10)).filter(docs_house_gov_postdate__gt=F('current_status_date'))
         sfs_bills = Bill.objects.filter(congress=CURRENT_CONGRESS, senate_floor_schedule_postdate__gt=datetime.datetime.now() - datetime.timedelta(days=5)).filter(senate_floor_schedule_postdate__gt=F('current_status_date'))
         coming_up = list(dhg_bills | sfs_bills)
         coming_up.sort(key = lambda b : b.docs_house_gov_postdate if (b.docs_house_gov_postdate and (not b.senate_floor_schedule_postdate or b.senate_floor_schedule_postdate < b.docs_house_gov_postdate)) else b.senate_floor_schedule_postdate, reverse=True)
-        
+
         start, end = get_congress_dates(CURRENT_CONGRESS)
         end_year = end.year if end.month > 1 else end.year-1 # count January finishes as the prev year
         current_congress_years = '%d-%d' % (start.year, end.year)
         current_congress = ordinal(CURRENT_CONGRESS)
-        
+
         return {
             "feeds": feeds,
-            
+
             "total": Bill.objects.filter(congress=CURRENT_CONGRESS).count(),
             "current_congress_years": current_congress_years,
             "current_congress": current_congress,
@@ -487,12 +528,12 @@ def bill_docket(request):
             "subjects": subject_choices(),
             "BILL_STATUS_INTRO": (BillStatus.introduced, BillStatus.referred, BillStatus.reported),
         }
-        
-    ret = cache.get("bill_docket_info")    
+
+    ret = cache.get("bill_docket_info")
     if not ret:
         ret = build_info()
         cache.set("bill_docket_info", ret, 60*60)
-    
+
     return ret
 
 @anonymous_view
@@ -517,7 +558,7 @@ def bill_statistics(request):
                   "link": "/congress/bills/browse?congress=%s&status=%s" % (c, ",".join(str(s) for s in g[4])),
                   } )
     counts_by_congress.reverse()
-    
+
     # When does activity occur within the session cycle?
     if settings.DATABASES['default']['ENGINE'] != 'django.db.backends.sqlite3':
         from django.db import connection
@@ -553,7 +594,7 @@ def subject(request, sluggedname, termid):
         ix1 = ix.parents.all()[0]
         ix2 = ix
     return show_bill_browse("bill/subject.html", request, ix1, ix2, { "term": ix, "feed": Feed.IssueFeed(ix) })
-    
+
 @user_view_for(subject)
 def subject_user_view(request, sluggedname, termid):
     ix = get_object_or_404(BillTerm, id=termid)
@@ -561,7 +602,7 @@ def subject_user_view(request, sluggedname, termid):
     from person.views import render_subscribe_inline
     ret.update(render_subscribe_inline(request, Feed.IssueFeed(ix)))
     return ret
-    
+
 import django.contrib.sitemaps
 class sitemap_current(django.contrib.sitemaps.Sitemap):
     changefreq = "weekly"
@@ -603,7 +644,7 @@ from django.contrib.auth.decorators import permission_required
 def go_to_summary_admin(request):
     summary, is_new = BillSummary.objects.get_or_create(bill=get_object_or_404(Bill, id=request.GET["bill"]))
     return HttpResponseRedirect("/admin/bill/billsummary/%d" % summary.id)
-    
+
 @anonymous_view
 @render_to('bill/uscode_index.html')
 def uscodeindex(request, secid):
@@ -614,16 +655,16 @@ def uscodeindex(request, secid):
         parent = get_object_or_404(USCSection, id=secid)
     else:
         parent = get_object_or_404(USCSection, citation="usc/" + secid)
-        
+
     children = USCSection.objects.filter(parent_section=parent).order_by('ordering')
-    
+
     from haystack.query import SearchQuerySet
     qs = SearchQuerySet().using("bill").filter(indexed_model_name__in=["Bill"])
     qs_current = qs.filter(congress=CURRENT_CONGRESS)
-    
+
     # How many bills cite this section?
     num_bills = qs_current.filter(usc_citations_uptree=parent.id).count() if parent else qs_current.count()
-    
+
     # Mark the children if we should allow the user to navigate there.
     # Only let them go to parts of the table of contents where there
     # are lots of bills to potentially track, at least historically.
@@ -632,7 +673,7 @@ def uscodeindex(request, secid):
     if children:
         for c in children:
             c.allow_navigation = c.num_bills > 5
-    
+
     return {
         "parent": parent,
         "children": children,
