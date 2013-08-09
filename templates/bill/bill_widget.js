e597788279800cdc85f8d785e8fb8709d2161620
Joshua Tauberer <jt@occams.info>
2013-08-09 09:25:43 -0400

    more tweaks for deplying the widget and getting it on the bill page

--- templates/bill/bill_widget.js
+++ templates/bill/bill_widget.js
@@ -2,9 +2,11 @@ var widgetIdPrefix = "govtrack:widget:bill:{{bill.congress}}:{{bill.bill_type_sl
 
 var widgetIframe = document.createElement("iframe");
 widgetIframe.id = widgetIdPrefix + ":iframe";
-widgetIframe.src = "{% if not debug %}{{SITE_ROOT_URL}}{% endif %}{{bill.get_absolute_url}}/widget.html";
+widgetIframe.src = "{% if not debug %}{{SITE_ROOT_URL}}{% else %}http://{{request.get_host}}{% endif %}{{bill.get_absolute_url}}/widget.html";
 widgetIframe.width = "500";
 widgetIframe.height = "200";
+widgetIframe.frameBorder = "0";
+widgetIframe.seamless = "seamless";
 
 var thisScript = document.scripts.namedItem(widgetIdPrefix + ":script");
 thisScript.parentNode.insertBefore(widgetIframe, thisScript);
