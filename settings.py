govtrack/govtrack.us-web

commit 9016b0938bf759a542bc10c9947278da956e0bf7

move from using MEDIA_URL to STATIC_URL for static resources
also deleting a dead file that used MEDIA_URL

settings.py
@@ -43,12 +43,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(ROOT, 'static')
+MEDIA_ROOT = os.path.join(ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
@@ -107,6 +107,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.humanize',
     'django.contrib.sitemaps',
+    'django.contrib.staticfiles',
+
     # 3rd party libraries
     'common',
     'django_extensions',

templates/bill/bill_statistics.html
@@ -6,7 +6,7 @@
 {% block cat-icon-filename %}img-bills-resolutions.png{% endblock %}
 
 {% block extra_js %}
-<script type="text/javascript" src="{{ MEDIA_URL }}js/highcharts.js" ></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/highcharts.js" ></script>
 {% endblock %}
 
 {% block crumbs %}

templates/bill/bill_widget.html
@@ -6,7 +6,7 @@
 	<meta charset="utf-8" />
 	<title>Bill Widget &ndash; {{bill.title_no_number}} ({{bill.display_number_no_congress_number}}{% if not bill.is_current %}, {{bill.congress|ordinal}} Congress{% endif %}) &ndash; GovTrack.us</title>
 
-	<link rel="stylesheet" href="{{ MEDIA_URL }}css/all{% if not debug %}-min{% endif %}.css?20130318" media="all" />
+	<link rel="stylesheet" href="{{ STATIC_URL }}css/all{% if not debug %}-min{% endif %}.css?20130318" media="all" />
 
 	<style type="text/css">
 		:root {
@@ -137,7 +137,7 @@
 
 
 	<div id="thelogo">
-		<a href="{{SITE_ROOT_URL}}" target="_blank"><img src="{{SITE_ROOT_URL}}{{ MEDIA_URL }}images/logo.gif" height="33px" alt="Powered by GovTrack.us" /></a>
+		<a href="{{SITE_ROOT_URL}}" target="_blank"><img src="{{SITE_ROOT_URL}}{{ STATIC_URL }}images/logo.gif" height="33px" alt="Powered by GovTrack.us" /></a>
 	</div>
 		
 </body>

templates/master.html
@@ -1,5 +1,5 @@
 {% load govtrack_utils %}
-{% with "/static/" as MEDIA_URL %} {% comment %}The 500 error page doesn't set this variable! {% endcomment %}
+{% with "/static/" as STATIC_URL %} {% comment %}The 500 error page doesn't set this variable! {% endcomment %}
 <!DOCTYPE html>
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>    <html class="lt-ie9 lt-ie8"> <![endif]-->
@@ -12,8 +12,8 @@
 
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
-		<link media="all" rel="stylesheet" href="{{ MEDIA_URL }}css/all{% if not debug %}-min{% endif %}.css?20131214" />
-		<!--[if lt IE 8]><link rel="stylesheet" type="text/css" href="{{ MEDIA_URL }}css/ie.css" media="screen" /><![endif]-->
+		<link media="all" rel="stylesheet" href="{{ STATIC_URL }}css/all{% if not debug %}-min{% endif %}.css?20131214" />
+		<!--[if lt IE 8]><link rel="stylesheet" type="text/css" href="{{ STATIC_URL }}css/ie.css" media="screen" /><![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<meta name="description" content="{% block meta_description %}{% endblock %}" />
@@ -33,7 +33,7 @@
 
 		{% block extra_css %}{% endblock %}
 
-		<script type="text/javascript" src="{{ MEDIA_URL }}js/modernizr.js"></script> {% comment %}html5shiv{% endcomment %}
+		<script type="text/javascript" src="{{ STATIC_URL }}js/modernizr.js"></script> {% comment %}html5shiv{% endcomment %}
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 		<script>
 			// when debugging off the network, fall back to a local copy of jquery
@@ -50,11 +50,11 @@
 		{% else %}
 			{% include "twostream/head.html" %}
 		{% endif %}
-		<script type="text/javascript" src="{{ MEDIA_URL }}js/jquery.cookie.js"></script>
-		<script src="{{ MEDIA_URL }}js/jquery.ba-bbq.min.js"></script>
-		<link rel="stylesheet" href="{{ MEDIA_URL }}js/colorbox/colorbox.css" />
-		<script src="{{ MEDIA_URL }}js/colorbox/jquery.colorbox-min.js"></script>
-		<script type="text/javascript" src="{{ MEDIA_URL }}js/jquery-utils.js?2013-07-07"></script>
+		<script type="text/javascript" src="{{ STATIC_URL }}js/jquery.cookie.js"></script>
+		<script src="{{ STATIC_URL }}js/jquery.ba-bbq.min.js"></script>
+		<link rel="stylesheet" href="{{ STATIC_URL }}js/colorbox/colorbox.css" />
+		<script src="{{ STATIC_URL }}js/colorbox/jquery.colorbox-min.js"></script>
+		<script type="text/javascript" src="{{ STATIC_URL }}js/jquery-utils.js?2013-07-07"></script>
 		{% block extra_js %}{% endblock %}
 		
 

templates/person/district_map.html
@@ -419,7 +419,7 @@
 
 	<div style="display: none">
 		<div id="fullscreen" style="padding: 10px">
-			<img src="{{ MEDIA_URL }}images/stock_fullscreen.png" title="Toggle Full Screen" onclick="doFullScreen();" style="cursor: pointer"/>
+			<img src="{{ STATIC_URL }}images/stock_fullscreen.png" title="Toggle Full Screen" onclick="doFullScreen();" style="cursor: pointer"/>
 		</div>
 	</div>
 	

templates/person/person_details.html
@@ -7,7 +7,7 @@
 {% block meta_keywords %}{{person.name_no_district}},legislation,bills,district,sponsor,cosponsor,votes,missed votes,absences,committees{% endblock %}
 
 {% block extra_js %}
-<script type="text/javascript" src="{{ MEDIA_URL }}js/highcharts.js" ></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/highcharts.js" ></script>
 {% endblock %}
 
 {% block body_scripts %}

templates/states/bill.html
@@ -11,7 +11,7 @@
 {% block meta_keywords %}{{bill.state_session.state}},legislation,bill,law{% endblock %}
 
 {% block head %}
-<link media="all" rel="stylesheet" href="{{ MEDIA_URL }}fonts/stateface.css">
+<link media="all" rel="stylesheet" href="{{ STATIC_URL }}fonts/stateface.css">
 {% endblock %}
 
 {% block crumbs %}

templates/states/bill_search.html
@@ -11,7 +11,7 @@
 {% block track_button_explanation %}Track major actions on all{% if state %} {{statename}}{% endif %} state bills{% if not state %} (nationwide){% endif %}.{% endblock %}
 
 {% block head %}
-<link media="all" rel="stylesheet" href="{{ MEDIA_URL }}fonts/stateface.css">
+<link media="all" rel="stylesheet" href="{{ STATIC_URL }}fonts/stateface.css">
 {% endblock %}
 
 {% block crumbs %}

commit 2b7e9840ba248d9b0242c56b6de43884362b3c3d

the change to STATIC_URL broke bill widgets in 9016b0938bf759a542bc10c9947278da956e0bf7
The bill widget HTML template seems to be the only template that doesn't
extend master.html. In master.html we explicitly define STATIC_URL because
Django's error views (500/400 pages) load the template without any context
variables. That seemed to make all of the other pages on the site work by
accident.

The fix is to add the staticfiles template context processor to settings.py
so that STATIC_URL gets set the right way. We still need the fix in
master.html because this is what the 500 pages don't load.

settings.py
@@ -145,6 +145,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'django.core.context_processors.static',
     'events.middleware.template_context_processor',
     'website.middleware.template_context_processor',
 )