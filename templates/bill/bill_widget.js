1406391c9553ce9eb26883ec3cc41f04285de6f0
Joshua Tauberer <jt@occams.info>
2013-12-25 08:16:33 -0500

    when debugging the bill widget use a URL that starts with // so that it can run either in http: or https:

--- templates/bill/bill_widget.js
+++ templates/bill/bill_widget.js
@@ -2,7 +2,7 @@ var widgetIdPrefix = "govtrack:widget:bill:{{bill.congress}}:{{bill.bill_type_sl
 
 var widgetIframe = document.createElement("iframe");
 widgetIframe.id = widgetIdPrefix + ":iframe";
-widgetIframe.src = "{% if not debug %}{{SITE_ROOT_URL}}{% else %}http://{{request.get_host}}{% endif %}{{bill.get_absolute_url}}/widget.html";
+widgetIframe.src = "{% if not debug %}{{SITE_ROOT_URL}}{% else %}//{{request.get_host}}{% endif %}{{bill.get_absolute_url}}/widget.html";
 widgetIframe.width = "500";
 widgetIframe.height = "200";
 widgetIframe.frameBorder = "0";
