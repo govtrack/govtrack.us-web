f1743bd7eb95643b377ae163be0c51ee88f91db8
Joshua Tauberer <jt@occams.info>
2014-05-12 09:43:59 -0400

    latest djang_twilio app breaks *args arguments to the view because they are absorbed by the methods and blacklist args on the decorator definition, but kwargs still work

--- poll_and_call/urls.py
+++ poll_and_call/urls.py
@@ -7,9 +7,9 @@ urlpatterns = patterns('poll_and_call.views',
     url(r'^([a-z0-9\-\_]+)/make_call$', 'issue_make_call'),
     url(r'^_ajax/start-call$', 'start_call'),
     url(r'^_ajax/poll-call-status$', 'poll_call_status'),
-    url(r'^_twilio/call-start/(\d+)$', 'twilio_call_start'),
-    url(r'^_twilio/call-input/(\d+)$', 'twilio_call_input'),
-    url(r'^_twilio/call-transfer-end/(\d+)$', 'twilio_call_transfer_ended'),
-    url(r'^_twilio/call-end/(\d+)$', 'twilio_call_end'),
+    url(r'^_twilio/call-start/(?P<calllog_id>\d+)$', 'twilio_call_start'),
+    url(r'^_twilio/call-input/(?P<calllog_id>\d+)$', 'twilio_call_input'),
+    url(r'^_twilio/call-transfer-end/(?P<calllog_id>\d+)$', 'twilio_call_transfer_ended'),
+    url(r'^_twilio/call-end/(?P<calllog_id>\d+)$', 'twilio_call_end'),
 )
 
