971bee07a25ff6660e7a426187709aeeb52eb2c0
Joshua Tauberer <jt@occams.info>
2014-05-12 11:41:11 -0400

    track the EOP segment in analytics

--- templates/user_head_script.js
+++ templates/user_head_script.js
@@ -1,4 +1,4 @@
-var the_segment = "{% if remote_net_house %}House{% elif remote_net_senate %}Senate{% elif is_dc_local %}DC{% else %}Other{% endif %}";
+var the_segment = "{% if remote_net_house %}House{% elif remote_net_senate %}Senate{% elif remote_net_eop %}EOP{% elif is_dc_local %}DC{% else %}Other{% endif %}";
 var cong_dist = {% if geolocation %}{{geolocation|safe}}{% else %}null{% endif %};
 var is_ad_free = {% if user.userprofile.paid_features.ad_free_life or user.userprofile.paid_features.ad_free_year %}true{% else %}false{% endif %};
 
