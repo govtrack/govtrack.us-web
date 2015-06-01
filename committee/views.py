govtrack/govtrack.us-web

commit 7e611987dde5ece3b732398efd2a94bfc97bc779

apply anonymous_view to committee pages

committee/views.py
@@ -11,6 +11,9 @@ from events.models import Feed
 
 from datetime import datetime
 
+from twostream.decorators import anonymous_view
+
+@anonymous_view
 @render_to('committee/committee_details.html')
 def committee_details(request, parent_code, child_code=None):
     if child_code:
@@ -44,6 +47,7 @@ def committee_details(request, parent_code, child_code=None):
             "party_counts": party_counts,
             }
 
+@anonymous_view
 @render_to('committee/committee_list.html')
 def committee_list(request):
     from events.models import Feed
@@ -73,6 +77,7 @@ class sitemap(django.contrib.sitemaps.Sitemap):
     def items(self):
         return Committee.objects.filter(obsolete=False)
 
+@anonymous_view
 @render_to("committee/calendar.html")
 def committee_calendar(request):
     committee_meetings = list(CommitteeMeeting.objects.filter(when__gte=datetime.now().date()).order_by()\

commit ee0551545ad70c5d07a1ec43e29afeed590007d2

making the committee details page anonymous in 7e611987dde5ece3b732398efd2a94bfc97bc779 broke the track button (the other pages dont use the track modal)

committee/views.py
@@ -11,7 +11,7 @@ from events.models import Feed
 
 from datetime import datetime
 
-from twostream.decorators import anonymous_view
+from twostream.decorators import anonymous_view, user_view_for
 
 @anonymous_view
 @render_to('committee/committee_details.html')
@@ -46,6 +46,18 @@ def committee_details(request, parent_code, child_code=None):
             "member_highlights": [m for m in members if m.role in (CommitteeMemberRole.chairman, CommitteeMemberRole.vice_chairman, CommitteeMemberRole.ranking_member)],
             "party_counts": party_counts,
             }
+
+
+@user_view_for(committee_details)
+def committee_details_user_view(request, parent_code, child_code=None):
+    committee = get_object_or_404(Committee, code=parent_code+(child_code if child_code else ""))
+
+    ret = { }
+
+    from person.views import render_subscribe_inline
+    ret.update(render_subscribe_inline(request, committee.get_feed()))
+
+    return ret
 
 @anonymous_view
 @render_to('committee/committee_list.html')

templates/committee/committee_details.html
@@ -76,8 +76,8 @@
 	
 
 	<div class="aside col-sm-4">
-		{% include "events/subscribe_inline.html" %}
 		<a href="#" class="track-btn" onclick="return show_track_panel();">Track Committee</a>
+		<div id="event_subscribe_inline_container"> </div>
 	</div><!-- /aside -->
 
 </div><!-- /container -->
@@ -183,7 +183,11 @@
 	</div><!-- /panes -->
 </div><!-- /.twelve.columns -->
 
-<script>$("ul.tabs li a").tabs("div.panes > div");</script>
-
 {% endblock %}
 
+{% block body_scripts %}
+<script>
+$("ul.tabs li a").tabs("div.panes > div");
+$('#event_subscribe_inline_container').html(the_page.events_subscribe_button);
+</script>
+{% endblock %}