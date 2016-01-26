8cd1b361e1b8861558067df7c74e1c08e81b7140
Joshua Tauberer <jt@occams.info>
2016-01-26 18:45:46 -0500

    pull the if.then.fund pop-up content from if.then.fund's new campaign JSON api

--- website/middleware.py
+++ website/middleware.py
@@ -80,6 +80,14 @@ def template_context_processor(request):
         cache.set("medium_posts", medium_posts, 60*15) # 15 minutes
     context["medium_posts"] = medium_posts
 
+    # Get a campaign from if.then.fund.
+    if_then_fund_campaign = cache.get("if_then_fund_campaign")
+    if not if_then_fund_campaign:
+        import urllib2, json
+        if_then_fund_campaign = json.load(urllib2.urlopen("https://if.then.fund/a/46.json"))
+    	cache.set("if_then_fund_campaign", if_then_fund_campaign, 60*45) # 45 minutes
+    context["if_then_fund_campaign"] = if_then_fund_campaign
+
     # Add context variables for whether the user is in the
     # House or Senate netblocks.
     
