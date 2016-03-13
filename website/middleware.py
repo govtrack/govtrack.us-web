54c385ba0b2283afe5a573fc0d3ecaf0dff921fe
Joshua Tauberer <jt@occams.info>
2016-03-13 17:05:39 -0400

    change the itf promo layout

--- website/middleware.py
+++ website/middleware.py
@@ -83,13 +83,13 @@ def template_context_processor(request):
 
     # Get a campaign from if.then.fund.
     itf_active_campaign = 49
-    if_then_fund_campaign = cache.get("if_then_fund_campaign")
+    if_then_fund_campaign = cache.get("if_then_fund_campaign_%d" % itf_active_campaign)
     if not if_then_fund_campaign and itf_active_campaign:
         try:
             if_then_fund_campaign = json.load(urllib2.urlopen("https://if.then.fund/a/%d.json" % itf_active_campaign))
         except:
             if_then_fund_campaign = "UHM" # something that is truthy otherwise we'll ping on every request
-    	cache.set("if_then_fund_campaign", if_then_fund_campaign, 60*45) # 45 minutes
+    	cache.set("if_then_fund_campaign_%d" % itf_active_campaign, if_then_fund_campaign, 60*45) # 45 minutes
     context["if_then_fund_campaign"] = if_then_fund_campaign
 
     # Add context variables for whether the user is in the
