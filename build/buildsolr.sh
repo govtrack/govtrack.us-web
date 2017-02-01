371839dd6c2c31e1c132d4720a45383de89bfd3d
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2017-02-01 10:34:07 -0500

    Add necessary solr/jetty config files

--- build/buildsolr.sh
+++ build/buildsolr.sh
@@ -6,13 +6,25 @@ tar xvzf solr-4.10.2.tgz
 cd solr-4.10.2
 
 # Create a new configuration from the example
-cp -R example govtrack_solr
+cp -R example govtrack
 
 # Initialize a collection for each of bill and person types
-mv solr/collection1 solr/bill
-echo "name=bill" > solr/bill/core.properties
-ln -s solr/bill/conf/schema.xml $GOVTRACK_ROOT/bill/solr/schema.xml
+mv govtrack/collection1 govtrack/bill
+echo "name=bill" > govtrack/bill/core.properties
+ln -s govtrack/bill/conf/schema.xml $GOVTRACK_ROOT/bill/solr/schema.xml
 
-cp -R solr/bill solr/person
-echo "name=person" > solr/person/core.properties
-ln -s solr/person/conf/schema.xml $GOVTRACK_ROOT/person/solr/schema.xml
+cp -R govtrack/bill govtrack/person
+echo "name=person" > govtrack/person/core.properties
+ln -s govtrack/person/conf/schema.xml $GOVTRACK_ROOT/person/solr/schema.xml
+
+# Copy Solr over to /opt
+sudo cp -R govtrack /opt/solr
+
+# Set up jetty to serve Solr
+sudo cp $GOVTRACK_ROOT/build/solrconfig/jetty /etc/default/jetty8
+sudo cp $GOVTRACK_ROOT/build/solrconfig/jetty-logging.xml /opt/solr/etc/jetty-logging.xml
+
+sudo useradd -d /opt/solr -s /sbin/false solr
+sudo mkdir /var/log/solr
+sudo chown solr:solr -R /opt/solr
+sudo chown solr:solr -R /var/log/solr
