ca5e8c1970e23d893c6ce45438146115de4e5b81
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2017-02-13 11:02:08 -0500

    Simplify the EC2 access in the fabfile

--- fabfile.py
+++ fabfile.py
@@ -19,44 +19,31 @@ import os
 #          solr  |            |               |
 #                +------------+---------------+
 
-def local_web_vm():
-    env.user = os.environ['FAB_WEB_USER']
-    env.hosts = os.environ['FAB_WEB_HOST'].split(',')
+def local_vm():
+    env.user = os.environ['VM_USER']
+    env.hosts = os.environ['VM_HOST'].split(',')
 
 
-def ec2_web_cluster():
-    """Borrowed with ♥ from http://joet3ch.com/blog/2012/01/18/fabric-ec2/"""
+def ec2_vm():
+    """
+    Target a single ec2 machine, or a cluster of machines, all with the group
+    name "govtrack-vm", for deployment.
+
+    Borrowed with ♥ from http://joet3ch.com/blog/2012/01/18/fabric-ec2/
+    """
 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    env.user = os.environ['FAB_WEB_USER']
-    env.key_filename = os.environ['FAB_WEB_KEY_FILENAME']
-    env.conftype = os.environ['FAB_WEB_CONFTYPE']
-    env.project = os.environ['FAB_WEB_PROJECT']
+    env.user = os.environ['VM_USER']
+    env.key_filename = os.environ['VM_KEY_FILENAME']
 
     ec2conn = ec2.connection.EC2Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-    web_group = ec2conn.get_all_security_groups(groupnames=['web'])
+    web_group = ec2conn.get_all_security_groups(groupnames=['govtrack-vm'])
     for i in web_group[0].instances():
         hostname = i.__dict__['public_dns_name']
         if hostname is not '':
             env.hosts.append(hostname)
-
-
-def local_solr_vm():
-    pass
-
-
-def ec2_solr_cluster():
-    pass
-
-
-def local_worker_vm():
-    pass
-
-
-def ec2_worker_cluster():
-    pass
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -67,7 +54,7 @@ def install_packages(update=True):
     if update:
         sudo('apt update')
 
-    sudo('apt install git python-virtualenv python-lxml python-openid \
+    sudo('apt install -y git python-virtualenv python-lxml python-openid \
               python-oauth2client python-iso8601 python-numpy python-scipy \
               python-prctl libssl-dev')
 
@@ -96,3 +83,11 @@ def deploy_web():
 
 def clean_web():
     run('rm -rf govtrack.us-web')
+
+
+def setenv(vars):
+    """
+    Read environment variables from string and update on remote machine. Restart
+    the services on the machine afterward.
+    """
+    pass
