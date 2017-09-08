584728f029173e967ef5527a709d3cb97f914d0b
WBT <wbt@users.noreply.github.com>
2017-09-08 00:15:22 -0400

    Add link directly to Bill from Position.

--- website/models.py
+++ website/models.py
@@ -320,6 +320,7 @@ class Reaction(models.Model):
 
 class Position(models.Model):
     subject = models.CharField(max_length=20, db_index=True)
+    bill = models.ForeignKey('bill.Bill', db_index=True, blank=True, null=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, blank=True, null=True, db_index=True, on_delete=models.CASCADE)
     anon_session_key = models.CharField(max_length=64, blank=True, null=True, db_index=True)
     position = models.IntegerField(blank=True, null=True)
@@ -327,9 +328,9 @@ class Position(models.Model):
     extra = JSONField()
 
     created = models.DateTimeField(auto_now_add=True, db_index=True)
-    
+
     class Meta:
-        unique_together = ( ('subject', 'user'), ('subject', 'anon_session_key') ) 
+        unique_together = ( ('subject', 'user'), ('subject', 'anon_session_key') )
 
     @staticmethod
     def get_session_key(request):
