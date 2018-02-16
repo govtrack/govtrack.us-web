6c93716928c4a0380d909ac8495459f61335abf9
Joshua Tauberer <jt@occams.info>
2018-02-16 14:49:45 -0500

    add administrator email addresses to stakeholders admin form

--- stakeholder/admin.py
+++ stakeholder/admin.py
@@ -2,11 +2,37 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django import forms
 
 from .models import *
 
+class AdminForm(forms.ModelForm):
+    class Meta:
+        model = Stakeholder.admins.through
+        fields = ['user']
+    email = forms.CharField(label='Email', disabled=True, required=False)
+    date_joined = forms.DateField(label="Date Joined", disabled=True, required=False)
+    last_login = forms.DateField(label="Last Login", disabled=True, required=False)
+    def __init__(self, *args, **kwargs):
+        if "instance" in kwargs:
+            kwargs = dict(kwargs) if kwargs else {}
+            kwargs.setdefault("initial", {})
+            for field in ("email", "date_joined", "last_login"):
+                kwargs["initial"][field] = getattr(kwargs["instance"].user, field)
+        super(AdminForm, self).__init__(*args, **kwargs)
+
+class StakeholderAdminsInline(admin.TabularInline):
+    model = Stakeholder.admins.through
+    raw_id_fields = ('stakeholder', 'user')
+    verbose_name = "Administrator"
+    verbose_name_plural = "Administrators"
+    extra = 0
+    form = AdminForm
+
 class StakeholderAdmin(admin.ModelAdmin):
     list_display = ('slug', 'name', 'verified')
-    raw_id_fields = ('admins',)
+    list_filter = ('verified',)
+    inlines = [StakeholderAdminsInline]
+    exclude = ('admins', 'extra')
 
 admin.site.register(Stakeholder, StakeholderAdmin)
