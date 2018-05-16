c214d9168f538563c02dc76e1a1601e8625fda63
Joshua Tauberer <jt@occams.info>
2018-05-16 15:30:19 -0400

    tweak vote export script

--- analysis/votes_export.py
+++ analysis/votes_export.py
@@ -5,23 +5,39 @@ from django.conf import settings
 
 from vote.models import *
 
+with_voters = False
+
 w = csv.writer(sys.stdout)
-w.writerow([
-	"congress", "session", "number", "date", "question", "link", "person", "vote"
-])
-for vote in Vote.objects.filter(congress=113, chamber=CongressChamber.house).order_by('created'):
-	voters = list(vote.voters.all().select_related('person', 'option'))
-	voters = sorted(voters, key = lambda v : v.person.sortname)
+w.writerow(
+	["congress", "session", "chamber", "number", "date", "category", "question", "link"]
+	+ (["person", "vote"] if with_voters else [])
+)
+
+votes = Vote.objects.filter(
+	#congress=113,
+	session__in=(2015, 2017),
+	chamber=CongressChamber.senate
+	).order_by('created')
+
+for vote in votes:
+	if with_voters:
+		voters = list(vote.voters.all().select_related('person', 'option'))
+		voters = sorted(voters, key = lambda v : v.person.sortname)
+	else:
+		voters = [None]
+
 	for voter in voters:
-		if voter.person_role.state != "AZ": continue
 		row = [
 				vote.congress,
 				vote.session,
+				vote.get_chamber_display(),
 				vote.number,
 				vote.created.strftime("%x %X"),
+				vote.get_category_display(),
 				vote.question,
 				settings.SITE_ROOT_URL + vote.get_absolute_url(),
+			] + ([
 				voter.person.sortname,
 				voter.option.value,
-			]
+			] if with_voters else [])
 		w.writerow([unicode(v).encode("utf8") for v in row])
