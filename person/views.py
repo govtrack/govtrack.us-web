govtrack/govtrack.us-web

commit a4130b8652e0192fac806c4187517acee706f67e

more py3 fixes

person/views.py
@@ -668,8 +668,8 @@ def person_session_stats_overview(request, session, cohort, specific_stat):
     for m in metrics:
         m["contexts"] = sorted(m["contexts"].values(), key = lambda c : -c["N"])
         for c in m["contexts"]:
-            c["people"][0].sort()
-            c["people"][1].sort()
+            c["people"][0].sort(key = lambda v : str(v))
+            c["people"][1].sort(key = lambda v : str(v))
 
     import dateutil.parser
     return {

bill/views.py
@@ -892,7 +892,7 @@ def bill_text_image(request, congress, type_slug, number, image_type):
 
     # Save to cache.
     if cache_fn:
-        with open(cache_fn, "w") as f:
+        with open(cache_fn, "wb") as f:
             f.write(imgbytes)
 
     # Return.

vote/views.py
@@ -234,12 +234,11 @@ def vote_export_csv(request, congress, session, chamber_code, number):
             voter.person_role.state if voter.person and voter.person_role else "--",
             voter.person_role.district if voter.person and voter.person_role else "--",
             voter.option.value,
-            voter.person.name_no_district().encode('utf-8') if voter.person else voter.get_voter_type_display(),
+            voter.person.name_no_district() if voter.person else voter.get_voter_type_display(),
             voter.person_role.party if voter.person and voter.person_role else "--",])
     output = outfile.getvalue()
     firstline = '%s Vote #%d %s - %s\n' % (vote.get_chamber_display(), vote.number,
                                          vote.created.isoformat(), vote.question) # strftime doesn't work on dates before 1900
-    firstline = firstline.encode('utf-8')
     r = HttpResponse(firstline + output, content_type='text/csv')
     r['Content-Disposition'] = 'attachment; filename=' + vote.get_absolute_url()[1:].replace("/", "_") + ".csv"
     return r

bill/billtext.py
@@ -320,7 +320,7 @@ def load_bill_text(bill, version, plain_text=False, mods_only=False, with_citati
         # convert XML on the fly to HTML
         import lxml.html, congressxml
         ret.update({
-            "text_html": lxml.html.tostring(congressxml.convert_xml(dat["xml_file"])),
+            "text_html": lxml.html.tostring(congressxml.convert_xml(dat["xml_file"]), encoding=str),
             "source": dat.get("xml_file_source"),
         })
 
@@ -328,13 +328,13 @@ def load_bill_text(bill, version, plain_text=False, mods_only=False, with_citati
         # This will be for bills around the 103rd-108th Congresses when
         # bill text is available from GPO but not in XML.
         ret.update({
-            "text_html": open(dat["html_file"]).read().decode("utf8"),
+            "text_html": open(dat["html_file"]).read(),
         })
 
     elif "text_file" in dat:
         # bill text from the Statutes at Large, or when plain_text is True then from GPO
 
-        bill_text_content = open(dat["text_file"]).read().decode("utf8")
+        bill_text_content = open(dat["text_file"]).read()
 
         # In the GPO BILLS collection, there's gunk at the top and bottom that we'd
         # rather just remove: metadata in brackets at the top, and <all> at the end.
@@ -428,7 +428,7 @@ def load_citation_info(metadata):
     def rebuild_usc_sec(seclist, indent=0):
         ret = []
         seclist = [kv for kv in seclist.items() if kv[0] != "_count"]
-        seclist = sorted(seclist, key=lambda x : x[0].ordering)
+        seclist = sorted(seclist, key=lambda x : x[0].ordering or -1)
         for sec, subparts in seclist:
             ret.append({
                 "text": (ucfirst(sec.level_type + ((" " + sec.number) if sec.number else "") + (": " if sec.name else "")) if sec.level_type else "") + (sec.name_recased if sec.name else ""),

bill/prognosis.py
@@ -516,8 +516,8 @@ def compute_prognosis_2(prognosis_model, bill, committee_membership, majority_pa
 			return model["success_rate"]
 		else:
 			factor_keys = set(f[0] for f in factors)
-			for key, index in model["regression_predictors_map"].items():
 			predictors = [0.0 for f in range(len(model["regression_beta"])-1)] # remove the intercept
+			for key, index in model["regression_predictors_map"].items():
 				predictors[index] = 1.0 if key in factor_keys else 0.0
 			return float(calcprob(model["regression_beta"], numpy.transpose(numpy.array([predictors]))))
 			

conf/uwsgi_start
@@ -1,6 +1,6 @@
 #!/bin/bash
 source .venv/bin/activate
-yuicompressor static/css/all.css > static/css/all-min.css
+./minify
 honcho -e local/settings.env run \
 	/home/govtrack/web/.venv/bin/uwsgi \
 	--wsgi-file wsgi.py \

parser/committee_parser.py
@@ -166,53 +166,53 @@ def main(options):
     loaded_meetings = set()
     processed_all_meetings = True
     for chamber in ("house", "senate"):
-		meetings_file = 'data/congress/committee_meetings_%s.json' % chamber
-		file_changed = File.objects.is_changed(meetings_file)
-	
-		if not file_changed and not options.force:
-			log.info('File %s was not changed' % meetings_file)
-			processed_all_meetings = False
-		else:
-			meetings = json.load(open(meetings_file))
-			
-			# Process committee event nodes
-			for meeting in meetings:
-				try:
-					# Associate it with an existing meeting object if GUID is already known.
-					# Must get it like this, vs just assigning the ID as we do in other parsers,
-					# because of the auto_now_add created field, which otherwise misbehaves.
-					try:
-						mobj = CommitteeMeeting.objects.get(guid=meeting['guid'])
-					except CommitteeMeeting.DoesNotExist:
-						mobj = CommitteeMeeting()
-					
-					# Parse.
-					mobj = meeting_processor.process(mobj, meeting)
-					
-					# Attach the meeting to the subcommittee if set.
-					if mobj.subcommittee:
-						mobj.committee = Committee.objects.get(code=mobj.committee.code + mobj.subcommittee)
-					
-					mobj.save()
-					loaded_meetings.add(mobj.id)
-					
-					mobj.bills.clear()
-					for bill in meeting["bill_ids"]:
-					    try:
-					        bill_type, bill_num, bill_cong = re.match(r"([a-z]+)(\d+)-(\d+)$", bill).groups()
-					        bill = Bill.objects.get(congress=bill_cong, bill_type=BillType.by_slug(bill_type), number=int(bill_num))
-					        mobj.bills.add(bill)
-					    except AttributeError:
-					        pass # regex failed
-					    except common.enum.NotFound:
-					        pass # invalid bill type code in source data
-					    except Bill.DoesNotExist:
-					        pass # we don't know about bill yet
-				except Committee.DoesNotExist:
-					log.error('Could not load Committee object for meeting %s' % meeting_processor.display_node(meeting))
-	
-			File.objects.save_file(meetings_file)
-		
+    	meetings_file = 'data/congress/committee_meetings_%s.json' % chamber
+    	file_changed = File.objects.is_changed(meetings_file)
+    
+    	if not file_changed and not options.force:
+    		log.info('File %s was not changed' % meetings_file)
+    		processed_all_meetings = False
+    	else:
+    		meetings = json.load(open(meetings_file))
+    		
+    		# Process committee event nodes
+    		for meeting in meetings:
+    			try:
+    				# Associate it with an existing meeting object if GUID is already known.
+    				# Must get it like this, vs just assigning the ID as we do in other parsers,
+    				# because of the auto_now_add created field, which otherwise misbehaves.
+    				try:
+    					mobj = CommitteeMeeting.objects.get(guid=meeting['guid'])
+    				except CommitteeMeeting.DoesNotExist:
+    					mobj = CommitteeMeeting()
+    				
+    				# Parse.
+    				mobj = meeting_processor.process(mobj, meeting)
+    				
+    				# Attach the meeting to the subcommittee if set.
+    				if mobj.subcommittee:
+    					mobj.committee = Committee.objects.get(code=mobj.committee.code + mobj.subcommittee)
+    				
+    				mobj.save()
+    				loaded_meetings.add(mobj.id)
+    				
+    				mobj.bills.clear()
+    				for bill in meeting["bill_ids"]:
+    				    try:
+    				        bill_type, bill_num, bill_cong = re.match(r"([a-z]+)(\d+)-(\d+)$", bill).groups()
+    				        bill = Bill.objects.get(congress=bill_cong, bill_type=BillType.by_slug(bill_type), number=int(bill_num))
+    				        mobj.bills.add(bill)
+    				    except AttributeError:
+    				        pass # regex failed
+    				    except common.enum.NotFound:
+    				        pass # invalid bill type code in source data
+    				    except Bill.DoesNotExist:
+    				        pass # we don't know about bill yet
+    			except Committee.DoesNotExist:
+    				log.error('Could not load Committee object for meeting %s' % meeting_processor.display_node(meeting))
+    
+    		File.objects.save_file(meetings_file)
+    	
     if processed_all_meetings:
         # Drop any future meetings that are no longer in the source data.
         obsolete_mtgs = CommitteeMeeting.objects.exclude(id__in=loaded_meetings).filter(when__gt=datetime.now())
@@ -224,7 +224,7 @@ def main(options):
         for committee in Committee.objects.filter(obsolete=False):
             log.info('Generating events for %s.' % committee)
             committee.create_events()
-				
+    			
 
 
 if __name__ == '__main__':

commit 6e260fafec8b1dd40f2f955def64db47cbd14536

a4130b86 broke sorting of reps in session stats overview pages

person/views.py
@@ -763,8 +763,8 @@ def person_session_stats_overview(request, session, cohort, specific_stat):
     for m in metrics:
         m["contexts"] = sorted(m["contexts"].values(), key = lambda c : -c["N"])
         for c in m["contexts"]:
-            c["people"][0].sort(key = lambda v : str(v))
-            c["people"][1].sort(key = lambda v : str(v))
+            c["people"][0].sort(key = lambda v : (v[0], v[2].sortname)) # sort by rank, then by name
+            c["people"][1].sort(key = lambda v : (v[0], v[2].sortname))
 
     import dateutil.parser
     return {