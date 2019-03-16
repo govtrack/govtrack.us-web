dfde9f1e81c461fd8fb4b7fb549c43675fa4fc87
Joshua Tauberer <tauberer@govtrack.us>
2019-03-16 12:59:31 +0000

    senate votes now show equivalent us population proportion

--- vote/models.py
+++ vote/models.py
@@ -13,6 +13,9 @@ from bill.models import BillSummary
 
 from website.templatetags.govtrack_utils import markdown
 
+global state_population
+state_population = None
+
 class CongressChamber(enum.Enum):
     senate = enum.Item(1, 'Senate')
     house = enum.Item(2, 'House')
@@ -407,6 +410,55 @@ class Vote(models.Model):
     def get_thumbnail_url(self):
         return self.get_absolute_url() + "/thumbnail"
 
+    def get_equivalent_aye_voters_us_population_percent(self):
+        # Only applicable for senate votes since we are summing state populations.
+        if self.chamber != CongressChamber.senate: return None
+
+        # Load historical population by state.
+        global state_population
+        if state_population is None:
+            import csv
+            state_population = { }
+            for state, year, pop in csv.reader(open("analysis/historical_state_population_by_year.csv")):
+                state_population[(state, int(year))] = int(pop)
+
+        # Clamp the year to the max year in the data, since the data lags real time.
+        year = min(self.created.year, max(year for state, year in state_population.keys()))
+
+        # Get the votes as a list of pairs holding (state, "+" | "-" | ...).
+        votes = self.voters\
+                      .filter(voter_type=VoterType.member)\
+                      .exclude(option__key='0')\
+                      .values_list('person_role__state', 'option__key')
+
+        # Count up the number of times each state appears. When there are no vacancies, each state occurs twice.
+        state_freq = { }
+        for state, vote in votes:
+            state_freq[state] = state_freq.get(state, 0) + 1
+
+        # Get the option key that had the most votes, prefering + over - if a tie.
+        vote_totals = { }
+        for state, vote in votes:
+            vote_totals[vote] = vote_totals.get(vote, 0) + 1
+        winner_option_key = sorted(vote_totals.items(), key = lambda kv : (-kv[1], kv[0]))[0][0]
+
+        # Sum the population of the winning votes and the population of all of the votes, dividing
+        # each voter's share of their state's population by the number of senators voting for
+        # that state, so that if there are vacancies we count each state equally.
+        winner = 0
+        total = 0
+        for state, vote in votes:
+            pop = state_population.get((state, year))
+            if pop is not None:
+               pop /= state_freq[state]
+               if vote == winner_option_key:
+                   winner += pop
+               total += pop
+        if total == 0:
+            return None
+
+        # Return the proportion of winner votes as well as which option we considered the winner.
+        return (winner_option_key, round(winner/total*100,1))
 
 class VoteOption(models.Model):
     vote = models.ForeignKey('vote.Vote', related_name='options', on_delete=models.CASCADE)
