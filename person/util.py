65819e995b3d25438c696f0e495113404586a765
Joshua Tauberer <jt@occams.info>
2020-01-06 23:40:07 -0500

    Simplify legislator subcommittee list

--- person/util.py
+++ person/util.py
@@ -11,7 +11,8 @@ def get_committee_assignments(person):
     in current congress.
 
     Returns sorted list of CommitteeMemberRole objects where each object is
-    committee assinment which could has subcommittee assignments in ``subroles`` attribute.
+    committee assignment which could has subcommittee assignments in ``subroles`` attribute,
+    and simple membership in a subcommittees list.
     """
 
     roles = person.committeeassignments.select_related('committee', 'committee__committee')
@@ -21,8 +22,9 @@ def get_committee_assignments(person):
             parent_mapping.setdefault(role.committee.committee_id, []).append(role)
     role_tree = []
     for role in roles:
-        if not role.committee.committee:
-            role.subroles = sort_members([x for x in parent_mapping.get(role.committee.pk, [])])
+        if not role.committee.committee: # is a main committee
+            role.subroles = sort_members([x for x in parent_mapping.get(role.committee.pk, []) if x.role not in (CommitteeMemberRole.member, CommitteeMemberRole.exofficio)])
+            role.subcommittees = sorted([x.committee for x in parent_mapping.get(role.committee.pk, []) if x.role in (CommitteeMemberRole.member, CommitteeMemberRole.exofficio)], key = lambda c : c.name_no_article)
             role_tree.append(role)
     role_tree = sort_members(role_tree)
     return role_tree
