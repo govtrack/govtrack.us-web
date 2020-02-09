a36bb4ef7cc51cf957f9f77e34a38bc91301ee21
Joshua Tauberer <tauberer@govtrack.us>
2020-02-09 14:25:02 +0000

    update some analysis scripts

--- analysis/amendments_passed_by_sponsor.py
+++ analysis/amendments_passed_by_sponsor.py
@@ -5,35 +5,48 @@ from glob import glob
 import os.path
 import json
 import csv, sys
+import tqdm
 
 import yaml
 
 from person.models import Person
 
-# Get current Members of Congress.
-people = yaml.load(open("data/congress-legislators/legislators-current.yaml"))
-people_map = {
-	person["id"]["thomas"]: Person.objects.get(id=person["id"]["govtrack"])
-	for person in people
-}
+CONGRESS_PROJECT_PATH = "../congress-project"
 
-people_map['01631'] = Person.objects.get(id=300022) # Hillary Clinton
-print(people_map['01631'])
+# Get a mapping from the old THOMAS IDs and Bioguide IDs to current Members of Congress.
+people = yaml.load(open(CONGRESS_PROJECT_PATH + "/congress-legislators/legislators-current.yaml"))
+people_map = { ("govtrack", key): value for (key, value) in Person.objects.in_bulk(person["id"]["govtrack"] for person in people).items() }
+for person in people:
+  for key, value in person["id"].items():
+    if key in ("thomas", "bioguide"):
+      people_map[(key, value)] = people_map[("govtrack", person["id"]["govtrack"])]
+
+#people_map['01631'] = Person.objects.get(id=300022) # Hillary Clinton, used this during the 2016 election season
+#print(people_map['01631'])
 
 # Count up by scanning files. Our in-database data is missing actions and
-# isn't well-linked to votes, at least in historical data.
+# isn't well-linked to votes, at least in historical data. First form a file list.
+amendment_file_list = glob(CONGRESS_PROJECT_PATH + "/data/*/amendments/*/*")
+
+# Scan the files.
 counts = defaultdict(lambda : defaultdict(lambda : 0))
 meta = defaultdict(lambda : defaultdict(lambda : None))
-for congress in range(93, 114+1):
-	for fn in glob("data/congress/%d/amendments/*/*" % congress):
+for fn in tqdm.tqdm(amendment_file_list):
 		with open(os.path.join(fn, 'data.json')) as f:
 			amdt = json.load(f)
 
 			if amdt["sponsor"]["type"] != "person": continue # amendments can be sponsored by committees
 
-			# Get sponsor & status.
-			sponsor = amdt["sponsor"]["thomas_id"]
-			if sponsor not in people_map: continue
+			# Get sponsor.
+			for id_type in ("thomas", "bioguide"):
+				if id_type + "_id" in amdt["sponsor"]:
+					sponsor = people_map.get( (id_type, amdt["sponsor"][id_type + "_id"]) )
+					if sponsor: break
+			else:
+				# Amendment's sponsor is not currently serving.
+				continue
+
+			# Get status.
 			status = amdt["status"]
 			if status not in ("offered", "pass", "fail", "withdrawn"): raise Exception()
 
@@ -61,10 +74,18 @@ for congress in range(93, 114+1):
 
 # Report.
 w = csv.writer(sys.stdout)
-for sponsor, results in sorted(counts.items(), key=lambda kv : kv[1]["passed"]):
+w.writerow([
+	"Sponsor ID",
+	"Sponsor Name",
+	"Earliest Amendment Vote Date",
+	"Latest Amendment Vote Date",
+	"Total",
+	"Passed"
+])
+for sponsor, results in sorted(counts.items(), key=lambda kv : kv[1]["passed"], reverse=True):
 	w.writerow([
-		people_map[sponsor].id,
-		people_map[sponsor].name.encode("utf8"), #sortname?
+		sponsor.id,
+		sponsor.name,
 		meta[sponsor]["first"],
 		meta[sponsor]["last"],
 		results["total"],
