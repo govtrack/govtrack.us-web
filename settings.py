7269c54dc17adbeb97fed136756a21aced82d915
Joshua Tauberer <tauberer@govtrack.us>
2020-04-11 18:07:49 +0000

    STATIC_URL wasn't working on the district map embed page
    d1250847324e9c0203ccd0d21f87f0979b6a62da broke it, probably, when jQuery was moved to a local static resource. Other templates seem to work maybe because master.html defines STATIC_URL itself with a 'with' template tag. Unclear if that's still necessary.

--- settings.py
+++ settings.py
@@ -143,6 +143,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
                 'events.middleware.template_context_processor',
                 'website.middleware.template_context_processor',
             ],
