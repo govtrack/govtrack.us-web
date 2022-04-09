c6986fb590a1b32f8cc54b31096032124bff3ba9
Joshua Tauberer <tauberer@govtrack.us>
2022-04-09 17:36:05 +0000

    Tweak missed_votes_3.py for finding 'present' votes

--- analysis/missed_votes_3.py
+++ analysis/missed_votes_3.py
@@ -2,6 +2,7 @@
 
 import sys, csv
 
+from django.db.models import Q
 from person.models import *
 from vote.models import *
 
@@ -16,10 +17,16 @@ if len(sys.argv) > 2:
 if len(sys.argv) > 3:
 	votes = votes.filter(vote__created__lte=sys.argv[3])
 
+# When searching on "Present" votes, only include votes were aye/no were possible
+# (i.e. exclude call by states and quorum calls etc).
+#substantive_votes = Vote.objects.filter(Q(options__key="+") | Q(options__key="-"))
+#votes = votes.filter(vote__in=substantive_votes)
+
 for vv in votes.values('person', 'person_role', 'option__key'):
 	d = stats.setdefault(vv['person'], { "total": 0, "count": 0, "role": None })
 	d['total'] += 1
 	if vv['option__key'] == "0":
+#	if vv['option__key'] == "P":
 		d['count'] += 1
 	d['role'] = vv['person_role']
 
