8f05c0c5754cf0eee7ae210f3578f1a328cb970e
Joshua Tauberer <tauberer@govtrack.us>
2022-09-22 19:09:26 +0000

    Add Rep. Shelley Moore Capito's middle name

--- person/name.py
+++ person/name.py
@@ -57,8 +57,9 @@ def get_person_name(person,
 
     # Others use their middle names with such regularity that it is hard
     # to recognize the legislator without it, but we'll only display it
-    # in first-last format.
-    elif person.id in (400204, 400295, 400659, 412293, 456814, ) and firstname_position == 'before':
+    # in first-last format. This is often maiden names.
+    elif person.id in (400204, 400295, 400659, 412293, 456814, 400061) \
+      and firstname_position == 'before':
         firstname += " " + person.middlename
 
     if person.nickname:
