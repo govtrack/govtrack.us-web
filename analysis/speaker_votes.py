afb65f43a223ed09f2f5c4cc23d1d44b015ddc30
Joshua Tauberer <tauberer@govtrack.us>
2023-01-05 11:13:23 +0000

    Refine the historical speaker vote analysis script looking for speakers that got votes from members of the other party

--- analysis/speaker_votes.py
+++ analysis/speaker_votes.py
@@ -22,19 +22,29 @@ for vote in Vote.objects.filter(question__icontains="Speaker", related_bill=None
 
 	# Find the parties of those that voted for the winner. Then take
 	# the sum of the votes from all but the first (highest count)
-	# party.
-	parties = vote.voters.filter(option__value=winner).values("person_role__party").annotate(count=Count('id')).order_by('-count')
-	cross_party_votes = sum(party['count'] for party in parties[1:])
+	# party. We can't use the party field in the database - instead we
+	# have to check the party on the date of the vote.
+	winner_votes_by_party = { }
+	for voter in vote.voters.filter(option__value=winner):
+		if voter.person_role is None: continue
+		party = voter.person_role.get_party_on_date(vote.created)
+		winner_votes_by_party[party] = winner_votes_by_party.get(party, 0) + 1
+	winner_votes_by_party = sorted(((v, k) for k, v in winner_votes_by_party.items()), reverse=True)
+	cross_party_votes = sum(v for v, k in list(winner_votes_by_party)[1:])
+
+	if cross_party_votes == 0: continue
 
 	print("""
 <!-- %s -->
-<tr><td><a href="%s">%s</a></td> <td>%s</td> <td>%s</td> <td><a href="%s">%s</a></td></tr>
+<tr><td><a href="%s">%s</a></td> <td>%s (%s)</td> <td>%s</td> <td><a href="%s">%s</a></td></tr>
 """ % (
 	vote.question,
 	"https://www.govtrack.us" + vote.get_absolute_url(),
 	vote.created.year,
+	winner,
 	options,
 	cross_party_votes,
 	vote.get_source_link(),
 	vote.get_source_display(),
 	))
+
